6cddf4117a172e6a66bd9910c809af9b2f8927ca
richardeveloper <richardjimenez.9641@outlook.com>
2022-10-26 19:14:49 -0500

    addMessageDowloadFormat

---
+++ js/dowloadFormat.js
@@ -0,0 +1,9 @@
+const dowload = document.getElementById("dowload");
+const dowloadTwo = document.getElementById("dowloadTwo");
+
+dowload.addEventListener('click', function () {
+    Swal.fire('¡EL PROCESO DE MATRICULACIÓN FINALIZÓ!')
+});
+dowloadTwo.addEventListener('click', function () {
+    Swal.fire('¡EL PROCESO DE MATRICULACIÓN FINALIZÓ!')
+});
