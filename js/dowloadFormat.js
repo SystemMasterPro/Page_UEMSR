dea1fbb9158c6bc51a69120ad9a32c89c4f70770
Richard <richardjimenez.9641@outlook.com>
2024-02-12 22:38:22 -0500

    updatePlaningAndFormats

--- js/dowloadFormat.js
+++ js/dowloadFormat.js
@@ -2,8 +2,8 @@ const dowload = document.getElementById("dowload");
 const dowloadTwo = document.getElementById("dowloadTwo");
 
 dowload.addEventListener('click', function () {
-    Swal.fire('¡DOCUMENTO HABILITADO EL 14/08/2023!')
+    Swal.fire('¡DOCUMENTO NO DISPONIBLE!')
 });
 dowloadTwo.addEventListener('click', function () {
-    Swal.fire('¡DOCUMENTO HABILITADO EL 14/08/2023!')
+    Swal.fire('¡DOCUMENTO NO DISPONIBLE!')
 });
